77e47674b62eceaced732a75958cd9ad185c7f7a
bigMeow <running@vip.163.com>
2019-08-25 19:27:46 +0800

    add remark

--- src/query.ts
+++ src/query.ts
@@ -15,6 +15,12 @@ const encode = (str: string) =>
 
 const decode = decodeURIComponent
 
+/**
+ * 将 'k1=v1&k2=v2' 格式的字符串转换成 query 对象
+ * @param query （可选）要转换的字符串，格式类似于 '?name=admin&password=123' 或者 'name=admin&password=123'
+ * @param extraQuery （可选）将转换后的 query 对象 附加到此 query 对象身上
+ * @param _parseQuery （可选）自定义转换函数
+ */
 export function resolveQuery(
   query?: string,
   extraQuery: Dictionary = {},
@@ -59,6 +65,10 @@ function parseQuery(query: string): Dictionary {
   return res
 }
 
+/**
+ * 将 query 对象序列化成 'k1=v1&k2=v2' 格式化的字符串
+ * @param obj
+ */
 export function stringifyQuery(obj: Dictionary): string {
   const res = obj
     ? Object.keys(obj)
