7620164c6dc6b6e0c840d01c26d995ca77a26619
lizong <lizong@jk724.com>
2020-04-09 16:16:10 +0800

    fix:

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { formatDate, formatMoney } from './format'
 export { validateBankCard, validateEmail, validateIDCard, validatePhoneNumber } from './validate'
 export { resolveQuery, stringifyQuery } from './query'
+export { uuid } from './uuid'
